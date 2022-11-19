1541f779e5d94cfe4cc0436f55c8ab98243c8105
JoseBlancoSiles <107679686+JoseBlancoSiles@users.noreply.github.com>
2022-11-19 23:30:13 +0100

    Delete users_table.sql

--- users_table.sql
+++
@@ -1,4 +0,0 @@
--- SQLite Creates table for our registants, USERNAME must be UNIQUE in order to prevent multiple registrations.
-
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(id INTEGER, username TEXT NOT NULL UNIQUE, email TEXT NOT NULL, password TEXT NOT NULL, PRIMARY KEY(id));
